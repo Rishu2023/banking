87b9f6d75f4eebc9711840fde466d550621ae325
Aryan Mittal <rishu108m@gmail.com>
2024-07-20 23:05:08 -0400

    Implement auth, appwrite, and sentry

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,13 +1,10 @@
 import HeaderBox from "@/components/ui/HeaderBox";
 import RightSidebar from "@/components/ui/RightSidebar";
 import TotalBalanceBox from "@/components/ui/TotalBalanceBox";
+import { getLoggedInUser } from "@/lib/actions/user.actions";
 
-const Home = () => {
-  const loggedIn = {
-    firstName: "Aryan",
-    lastName: "Mittal",
-    email: "rishu108m@gmail.com",
-  };
+const Home = async  () => {
+  const loggedIn = await getLoggedInUser();
   return (
     <section className="home">
       <div className="home-content">
@@ -15,7 +12,7 @@ const Home = () => {
           <HeaderBox
             type="greeting"
             title="Welcome"
-            user={loggedIn?.firstName || "Guest"}
+            user={loggedIn?.name || "Guest"}
             subtext="Access and manage your account and transactions efficiently."
           />
 
